9dfda1879b409d5e6f867fe8a03f28aecc673f96
Dipta04 <diptakarmakar38@gmail.com>
2022-11-26 06:39:54 +0600

    isSeller

--- index.js
+++ index.js
@@ -199,6 +199,13 @@ async function run() {
          res.send(result);
       })
 
+      app.get('/users/seller/:email', async (req, res) => {
+         const email = req.params.email;
+         const query = { email }
+         const user = await usersCollection.findOne(query);
+         res.send({ isSeller: user?.role === 'seller' });
+      })
+
 
    }
    finally {
